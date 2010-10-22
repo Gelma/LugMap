cf82051b8410e2d6115a3727fccfc90652e04437
Andrea Gelmini <andrea.gelmini@gelma.net>
2010-10-22 15:10:53 +0200

    Fix vari per evitare falsi positivi.
    Aggiunto anche micro debug

--- spazzino.py
+++ spazzino.py
@@ -15,27 +15,36 @@ def controllo_dominio_dns(url):
 	"""Ricevo un URL, estraggo il dominio, torno True se tutto torna. Diversamente emetto il messaggio da segnalare"""
 
 	url = url.split('/')[2] # estraggo solo il nome di dominio	# TODO: una regexp sarebbe più elegante
+	if Debug: print "controllo_dominio_dns: dominio estrapolato",url
 
 	try:
 		info_url = socket.getaddrinfo(url, 80, 0, 0, socket.SOL_TCP)
 	except:
+		if Debug: print "controllo_dominio_dns: socket.getaddrinfo fallito"
 		return "Errore: problema sul dominio (esistenza/mappatura)"
 
 	IP_nuovo = info_url[0][4][0] # Inizio controllo IP cambiato. Possono esserci dei falsi positivi in caso di round robin.
+	if Debug: print "controllo_dominio_dns: IP_nuovo", IP_nuovo
 
-	try: # Pesco il vecchio iP
+	try: # Pesco i vecchi iP
 		IP_vecchi = archivio[url_completo]['IP']
+		if Debug: print "controllo_dominio_dns: IP_vecchi",IP_vecchi
 	except: # Non esiste ancora una voce relativa, la creiamo
 		archivio[url_completo] = {'IP': set([IP_nuovo])}
+		if Debug: print "controllo_dominio_dns: IP_vecchi inesistenti"
 		return True
 
 	if IP_nuovo in IP_vecchi: # se è gia' presente, me ne fotto
+		if Debug: print "controllo_dominio_dns: IP noto", IP_nuovo
 		return True
 	else: # diversamente segnalo e aggiungo
-		IP_vecchi.add(IP_nuovo)
-		archivio[url_completo]['IP'] = IP_vecchi
+		elenco_ip_dns = IP_vecchi
+		if Debug: print "controllo_dominio_dns: elenco vecchi IP", elenco_ip_dns
+		elenco_ip_dns.add(IP_nuovo)
+		archivio[url_completo]['IP'] = elenco_ip_dns
+		if Debug: print "controllo_dominio_dns: elenco IP storati", archivio[url_completo]['IP']
 		archivio.sync()
-		return 'Attenzione: cambiato IP del server (Vecchio: %s, Nuovo: %s)' % (IP_vecchi, IP_nuovo)
+		return 'Attenzione: cambiato IP del server (Vecchio: %s, Nuovo: %s, Storato: %s)' % (' - '.join(IP_vecchi), IP_nuovo, ' - '.join(elenco_ip_dns))
 
 def controllo_contenuto():
 	"""Leggo lo URL elaborato, estraggo la pagina, ne valuto la differenza rispetto alla lettura precedente. Torno True se tutto a posto."""
@@ -56,8 +65,9 @@ def controllo_contenuto():
 	valore_magico = float(len(Termini_Precedenti.intersection(Termini_Attuali))*1.0/len(Termini_Precedenti.union(Termini_Attuali)))
 	archivio[url_completo] = {'TerminiPrecedenti': Termini_Attuali}
 
+	if Debug: print "controllo_contenuto: valore_magico = ",str(valore_magico)
 	if valore_magico <= 0.7:
-		return 'Errore: troppa differenza di contenuto:'+str(valore_magico)
+		return 'Errore: troppa differenza di contenuto:',str(valore_magico)
 	else:
 		return True
 	
@@ -67,7 +77,8 @@ def richiedi_controllo(errore):
 	email_alert = 'andrea.gelmini@gmail.com'
 	
 	if os.path.exists('/usr/bin/mail'):
-		echo_command = shlex.split("echo '"+'\n'.join([errore]+riga)+"'")
+		#echo_command = shlex.split("echo '"+'\n'.join([errore]+riga)+"'")
+		echo_command = shlex.split("echo '"+errore+"'")
 		mail_command = shlex.split("mail -s 'LugMap check: %s' %s" % (riga[3], email_alert))
 		subprocess.Popen(mail_command, stdin=subprocess.Popen(echo_command, stdout=subprocess.PIPE).stdout, stdout=subprocess.PIPE).wait()
 
@@ -82,6 +93,7 @@ if __name__ == "__main__":
 	#															['IP'] = Set degli IP
 	#															['TerminiPrecedenti'] = Set dei Termini delle pagine HTML
 
+	Debug = True
 	while True:
 		print 'Nuovo giro'
 		syslog.syslog(syslog.LOG_ERR, 'Spazzino: Nuovo giro')
@@ -95,10 +107,10 @@ if __name__ == "__main__":
 				responso = controllo_dominio_dns(url_completo) # inizio il ciclo di controlli
 				if responso is not True:
 					richiedi_controllo(responso)
-	
-				responso = controllo_contenuto()
-				if responso is not True:
-					richiedi_controllo(responso)
+				else:
+					responso = controllo_contenuto()
+					if responso is not True:
+						richiedi_controllo(responso)
 					
 		archivio.sync()
 		archivio.close()
