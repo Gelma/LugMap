dc58555032cc021019e43f49c5fa9ae1e826968f
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-01-27 14:32:55 +0100

    Espanso note sulla collaborazione

--- index.php
+++ index.php
@@ -240,10 +240,10 @@ require_once ('varie.php');
 								Questa LugMap è frutto di un lavoro collettivo e collaborativo, cui tutti sono invitati a partecipare con suggerimenti e segnalazioni.
 							</p>
 							<p>
-								Il sito viene periodicamente aggiornato sulla base di un <a href="https://github.com/Gelma/LugMap">repository GitHub</a> pubblicamente leggibile, in cui le informazioni sui LUG sono organizzate in files direttamente accessibili (ad esempio: http://github.com/Gelma/LugMap/blob/master/db/nome_regione.txt) e facilmente parsabili da applicazioni esterne.
+								Il sito viene periodicamente aggiornato sulla base di un <a href="https://github.com/Gelma/LugMap">repository GitHub</a> pubblicamente leggibile, in cui le informazioni sui LUG sono organizzate in <a href="http://github.com/Gelma/LugMap/blob/master/db/">file direttamente accessibili</a>, e comodamente fruibili da terzi alla stregua di file CSV.
 							</p>
 							<p>
-								Segnalaci nuovi/vecchi LUG, così come eventuali correzioni ed errori, mandando una mail a <a href="mailto:lugmap@linux.it">lugmap@linux.it</a>.
+								Segnalaci nuovi/vecchi LUG, così come eventuali correzioni ed errori, mandando una mail a <a href="mailto:lugmap@linux.it">lugmap@linux.it</a>. Ancor meglio, se sei interessato, puoi avere accesso allo stesso <a href="https://github.com/Gelma/LugMap">repository</a> per partecipare attivamente all'aggiornamento dei contenuti. Il tuo contributo ha molteplici effetti: questi dati sono la stessa fonte della <a href="http://www.linux.it/LUG">LugMap di ILS</a>, da cui diverse riviste ricavano le informazioni da pubblicare.
 							</p>
 						</div>
 					</div>
