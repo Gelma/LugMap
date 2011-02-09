c4fa060840e4d3f8acf0a81ea8be11ff15966602
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-02-09 17:20:19 +0100

    Controlli portati nella classe Lug

--- spazzino.py
+++ spazzino.py
@@ -8,73 +8,73 @@ if True: # import dei moduli
 		import copy, csv, glob, os, shelve, socket, sys, smtplib, syslog, urllib
 	except:
 		import sys
-		print "Non sono disponibili tutti i moduli necessari."
+		print "Non sono disponibili tutti i moduli standard necessari."
 		sys.exit(-1)
-
-def controllo_dominio_dns(url):
-	"""Ricevo un URL, estraggo il dominio, torno True se tutto torna, diversamente False"""
-
-	url = url.split('/')[2] # estraggo solo il nome di dominio	# TODO: una regexp sarebbe più elegante
-	if Debug: print "\t--->",url
-
 	try:
-		info_url = socket.getaddrinfo(url, 80, 0, 0, socket.SOL_TCP)
+		import ZODB, persistent, transaction
 	except:
-		if Debug: print "controllo_dominio_dns: socket.getaddrinfo fallito"
-		email_errori.aggiungi("Errore: problema sul dominio (esistenza/mappatura)")
-		return False
+		print "Non sono disponibili uno o più di questi moduli: ZODB, persistent, transaction"
 
-	IP_nuovo = info_url[0][4][0] # Inizio controllo IP cambiato. Possono esserci dei falsi positivi in caso di round robin.
-	if Debug: print "controllo_dominio_dns: IP_nuovo", IP_nuovo
+if True: # attiva DB
+	from ZODB.FileStorage import FileStorage
+	from ZODB.DB import DB
+	storage = FileStorage(os.path.join(os.environ["HOME"], '.spazzino.db'))
+	db = DB(storage)
+	connection = db.open()
+	pdb = connection.root()
 
-	try: # Pesco i vecchi iP
-		IP_vecchi = copy.deepcopy(archivio[url_completo]['IP'])
-		if Debug: print "controllo_dominio_dns: IP_vecchi",IP_vecchi
-	except: # Non esiste ancora una voce relativa, la creiamo
-		archivio[url_completo] = {'IP': set([IP_nuovo])}
-		if Debug: print "controllo_dominio_dns: IP_vecchi inesistenti"
-		return True
+class Lug(persistent.Persistent):
+	def __init__(self, url_del_lug):
+		self.url = url_del_lug
+		self.reset_iniziale()
+		self.dominio = url_del_lug.split('/')[2]
+		self.Termini_Precedenti = set()
+		self.DNS_noti = set()
 
-	if IP_nuovo in IP_vecchi: # se è gia' presente, me ne fotto
-		if Debug: print "controllo_dominio_dns: IP noto", IP_nuovo
-		return True
-	else: # diversamente segnalo e aggiungo
-		elenco_ip_dns = IP_vecchi
-		if Debug: print "controllo_dominio_dns: elenco vecchi IP", elenco_ip_dns
-		elenco_ip_dns.add(IP_nuovo)
-		archivio[url_completo]['IP'] = elenco_ip_dns
-		if Debug: print "controllo_dominio_dns: elenco IP storati", archivio[url_completo]['IP']
-		archivio.sync()
-		dettaglio = 'Attenzione: cambiato IP del server (Vecchio: %s, Nuovo: %s, Storato: %s)' % (' - '.join(IP_vecchi), IP_nuovo, ' - '.join(elenco_ip_dns))
-		email_errori.aggiungi(dettaglio)
-		return False
+	def reset_iniziale(self):
+		self.email_errori = email_report()
 
-def controllo_contenuto():
-	"""Leggo lo URL elaborato, estraggo la pagina, ne valuto la differenza rispetto alla lettura precedente. Torno True se tutto a posto."""
+	def controllo_dns(self):
+		"""Controllo l'esistenza e la mappatura del dominio"""
 
-	try: # pesco la pagina
-		pagina_html = urllib.urlopen(url_completo).read()
-	except:
-		email_errori.aggiungi('Errore: impossibile leggere la pagina html.')
-		return False
+		print "Controllo dominio",self.dominio
+		try:
+			DNS_attuale = socket.getaddrinfo(self.dominio, 80, 0, 0, socket.SOL_TCP)[0][4][0]
+		except:
+			print "socket.getaddrinfo fallito"
+			self.email_errori.aggiungi("Errore: problema sul dominio (esistenza/mappatura)")
+			return False
 
-	Termini_Attuali = set(pagina_html.split()) # Estrapolo subito i termini presenti
+		if DNS_attuale not in self.DNS_noti:
+			dettaglio = 'Attenzione: nuovo IP %s (%s)' % (DNS_attuale,' '.join(self.DNS_noti))
+			print dettaglio
+			self.DNS_noti.add(DNS_attuale)
+			self.email_errori.aggiungi(dettaglio)
+			return False
 
-	try: # Leggo i termini precedenti
-		Termini_Precedenti = archivio[url_completo]['TerminiPrecedenti']
-	except: # Non esiste la voce, la creo
-		archivio[url_completo]['TerminiPrecedenti'] = Termini_Attuali
-		return True
+	def controllo_contenuto(self):
+		"""Leggo lo URL e faccio una valutazione numerica. True/False di ritorno."""
 
-	valore_magico = float(len(Termini_Precedenti.intersection(Termini_Attuali))*1.0/len(Termini_Precedenti.union(Termini_Attuali)))
-	archivio[url_completo] = {'TerminiPrecedenti': Termini_Attuali}
+		try: # pesco la pagina
+			pagina_html = urllib.urlopen(self.url).read()
+		except:
+			self.email_errori.aggiungi('Errore: impossibile leggere la pagina html.')
+			return False
 
-	if Debug: print "controllo_contenuto: valore_magico = ",str(valore_magico)
-	if valore_magico <= 0.7:
-		email_errori.aggiungi('Errore: troppa differenza di contenuto:'+str(valore_magico))
-		return False
-	else:
-		return True
+		Termini_Attuali = set(pagina_html.split()) # Estrapolo i termini presenti
+		valore_magico = \
+		  float(len(self.Termini_Precedenti.intersection(Termini_Attuali))*1.0/len(self.Termini_Precedenti.union(Termini_Attuali)))
+		self.Termini_Precedenti = Termini_Attuali
+
+		print "controllo_contenuto: valore_magico = ",str(valore_magico)
+		if valore_magico <= 0.7:
+			self.email_errori.aggiungi('Errore: troppa differenza di contenuto:'+str(valore_magico))
+			return False
+
+	def invia_report(self):
+		self.email_errori.subject = 'LugMap: '+self.url
+		self.email_errori.invia()
+
 
 class email_report():
 	"""Prendo in pasto errori e li invio via SMTP"""
@@ -121,25 +121,12 @@ class email_report():
 		if Debug: print self.subject,self.righe
 		syslog.syslog(syslog.LOG_ERR, 'Spazzino: '+self.subject+' '+'  '.join(self.righe))
 
+
 if __name__ == "__main__":
-	# La struttura dell'archivio è:
-	# dizionario archivio[url_completo come chiave]: dizionario
-	#		['IP'] = Set degli IP
-	#		['TerminiPrecedenti'] = Set dei Termini delle pagine HTML
-
-	Debug = True
-
-	archivio = shelve.open(os.path.join(os.environ["HOME"], '.spazzino.db'), writeback=True) # Apro il db persistente
-
 	for filedb in glob.glob( os.path.join('./db/', '*.txt') ): # piglio ogni file db
 		for riga in csv.reader(open(filedb, "r"), delimiter='|', quoting=csv.QUOTE_NONE): # e per ogni riga/Lug indicato
-			url_completo = riga[3]
-
-			email_errori = email_report()
-			email_errori.subject = 'Lugmap: '+url_completo
-
-			if controllo_dominio_dns(url_completo): # controllo mappature
-				responso = controllo_contenuto() # se ok, controllo anche il contenuto
-			email_errori.invia()
-	archivio.sync()
-	archivio.close()
+			lug = Lug(riga[3]) # Creo l'istanza passandogli l'url completo
+			if lug.controllo_dns():
+				print "DNs ok"
+			else:
+				print "scazzo dns"
