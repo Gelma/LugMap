be12883ddc6cf5d915de8bd07735baf3f11319da
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-03-23 16:40:07 +0100

    File per l'aggiornamento automatico sul server di ILS

---
+++ Aggiorna.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+fallito_aggiornamento() {
+	# segnalo via mail problemi sull'aggiornamento, se possibile
+	[ -e /usr/sbin/sendmail ] && echo "Problema aggiornamento git-pull lugmap.linux.it" | /usr/sbin/sendmail -s "LugMap.linux.it: errore git-pull" lugmap@lists.linux.it
+	# sputo qualcosa anche in output, contando che venga intercettato da cron.
+	echo "LugMap.linux.it: errore git-pull"
+}
+
+/usr/bin/git pull -q git@github.com:Gelma/LugMap.git lugmap.linux.it || fallito_aggiornamento
