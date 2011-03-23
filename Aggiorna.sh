3802e1501952cfc19f562464707c041ee5a94dbe
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-03-23 16:55:24 +0100

    Controllo percorso

--- Aggiorna.sh
+++ Aggiorna.sh
@@ -5,6 +5,8 @@ fallito_aggiornamento() {
 	[ -e /usr/sbin/sendmail ] && echo "Problema aggiornamento git-pull lugmap.linux.it" | /usr/sbin/sendmail -s "LugMap.linux.it: errore git-pull" lugmap@lists.linux.it
 	# sputo qualcosa anche in output, contando che venga intercettato da cron.
 	echo "LugMap.linux.it: errore git-pull"
+	exit
 }
 
+cd $PATH || fallito_aggiornamento
 /usr/bin/git pull -q git@github.com:Gelma/LugMap.git lugmap.linux.it || fallito_aggiornamento
