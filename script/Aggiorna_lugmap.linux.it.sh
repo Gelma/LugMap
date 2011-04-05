384a947f8c1471c3791d7d7b8081e47b81e8ec27
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-04-05 16:37:56 +0200

    Integrato modifiche di Fabio Invernizzi <fabulus@linux.it>

--- script/Aggiorna_lugmap.linux.it.sh
+++ script/Aggiorna_lugmap.linux.it.sh
@@ -4,12 +4,10 @@ PATH_SITO='/var/www/lugmap'
 
 fallito_aggiornamento() {
 	# segnalo via mail problemi sull'aggiornamento, se possibile
-	[ -e /usr/sbin/sendmail ] && echo "Problema aggiornamento git-pull lugmap.linux.it" | /usr/sbin/sendmail -s "LugMap.linux.it: errore git-pull" fabulus@linux.it
-	[ -e /usr/bin/mail ] && echo "Problema aggiornamento git-pull lugmap.linux.it" | /usr/bin/mail -s "LugMap.linux.it: errore git-pull" fabulus@linux.it
+	[ -e /usr/bin/mail ] && echo "Problema aggiornamento git-pull lugmap.linux.it" | /usr/bin/mail -s "LugMap.linux.it: errore git-pull" lugmap@lists.linux.it
 	# sputo qualcosa anche in output, contando che venga intercettato da cron.
 	echo "LugMap.linux.it: errore git-pull"
 	exit
 }
 
-cd $PATH_SITO || fallito_aggiornamento
-/usr/bin/git pull -q git://github.com/Gelma/LugMap.git lugmap.linux.it || fallito_aggiornamento
+su -c "cd $PATH_SITO && /usr/bin/git pull -q git://github.com/Gelma/LugMap.git lugmap.linux.it" www-data || fallito_aggiornamento
