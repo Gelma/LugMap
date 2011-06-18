ffdf92a6239f908e06e5d78aff59aee279e52671
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-06-18 23:31:31 +0200

    Aggiunta licenza alle procedure
    Non che ce ne fosse davvero bisogno, vista la stupidita' delle stesse,
    ma faceva antipatia non rilasciarle con le opportune licenze.

--- script/WiMegera/WiMegera.py
+++ script/WiMegera/WiMegera.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Leggo la cronologia di questa pagina Wikipedia:
-http://it.wikipedia.org/wiki/Lista_dei_LUG_italiani
-e notifico eventuali variazioni alla LugMap"""
+	http://it.wikipedia.org/wiki/Lista_dei_LUG_italiani
+	e notifico eventuali variazioni alla LugMap
+
+	Copyright 2010-2011 - Andrea Gelmini (andrea.gelmini@gelma.net)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
 import editarticle, os, pickle
 
