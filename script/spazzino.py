f57c7616151e87237dea8a6a71f8260a4195d9fd
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-07-04 01:44:09 +0200

    Spazzino: vediamo se stavolta l'ho scritto correttamente

--- script/spazzino.py
+++ script/spazzino.py
@@ -114,7 +114,7 @@ class Lug(persistent.Persistent):
 		try:
 			if self.titolo != titolo_attuale:
 				print "Divergenza", self.titolo, titolo_attuale
-				self.email_errori.aggiunti('      Errore: title della home cambiato da ' +self.titolo+' a '+titolo_attuale)
+				self.email_errori.aggiungi('      Errore: title della home cambiato da ' +self.titolo+' a '+titolo_attuale)
 				self.numero_errori += 1
 				self.titolo = titolo_attuale
 				return False
