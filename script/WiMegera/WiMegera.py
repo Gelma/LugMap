83b725f916d72ef57975877eb194eab7db92cb0d
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-07-22 23:54:31 +0200

    WiMegera: aggiunto supporto multipagina

--- script/WiMegera/WiMegera.py
+++ script/WiMegera/WiMegera.py
@@ -19,52 +19,50 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
-import editarticle, notifiche, os, pickle, smtplib
+try:
+	import editarticle, notifiche, os, pickle, shelve
+except:
+	import sys
+	sys.exit('Errore: non sono disponibili tutti i moduli necessari')
 
 class AccessoCronologia(editarticle.ArticleEditor):
 	def run(self):
 		return self.page.getVersionHistory(self)
 
-def leggo_ultimo_id_inviato():
-
-	try:
-		return pickle.load(open(file_con_ultima_modifica, 'r'))
-	except:
-		return 0
-
-def salva_ultimo_id_inviato(id):
-	pickle.dump(id, open(file_con_ultima_modifica, 'w'))
-
 if __name__ == "__main__":
+	db = shelve.open(os.path.join(os.environ["HOME"], '.wimegera.db'), writeback=True) # Apro DB
 
 	# Variabili globali
-	file_con_ultima_modifica = os.path.join(os.environ["HOME"], '.wimegera_ultimoid')
 	righe_della_mail = []
+	pagine_wikipedia_da_controllare = ('Lista_dei_LUG_italiani', 'Hacklab') # eventuali altre pagine da monitorare vanno aggiunte qui
 
-	ultimo_id_inviato = leggo_ultimo_id_inviato()
+	for pagina_wiki in pagine_wikipedia_da_controllare: # prelevo la lista di modifiche
+		cronologia = AccessoCronologia('-p', pagina_wiki)
+		elenco_modifiche = [x for x in cronologia.run()]#¹
+		elenco_modifiche.reverse()
 
-	oggetto_modifiche = AccessoCronologia('-p','Lista_dei_LUG_italiani')
-	elenco_modifiche = [x for x in oggetto_modifiche.run()]#¹
-	elenco_modifiche.reverse()
+		for modifica in elenco_modifiche: # controllo ogni modifica
+			id, data, autore, nota = [modifica[x] for x in range(4)]
+			try: # provo il confronto
+				if int(id) > db[pagina_wiki]:
+					db[pagina_wiki] = int(id)
+					righe_della_mail.append('Nota: '  +nota)
+					righe_della_mail.append('Data: '  +data+'   Autore: '+autore+'   ID: '+str(id))
+					righe_della_mail.append('\n'+15*'-_'+'\n')
+			except KeyError: # se non trovo la pagina nel db, significa che è nuova
+				db[pagina_wiki] = 0
+				righe_della_mail.append('Nota: pagina nuova')
 
-	for modifica in elenco_modifiche:
-		id, data, autore, nota = [modifica[x] for x in range(4)]
-		if int(id) > ultimo_id_inviato:
-			righe_della_mail.append('Nota: '  +nota)
-			righe_della_mail.append('Data: '  +data+'   Autore: '+autore+'   ID: '+str(id))
-			righe_della_mail.append('\n'+15*'-_'+'\n')
+		if righe_della_mail:
+			try:
+				mail = notifiche.email(mittente		= 'WiMegera <wimegera@gelma.net>',
+									   destinatario	= ['WiMegera <wimegera@gelma.net>'],
+									   oggetto 		= 'WiMegera: aggiornamento su '+pagina_wiki,
+									   testo		= righe_della_mail,
+									   invia_subito	= True) # Se da Aggiornare, vedi Guida Intergalattica alla LugMap §4.1
+			except: # se fallisce l'invio stampo la mail, contando sul delivery di cron
+				print '\n'.join(righe_della_mail)
 
-	if righe_della_mail:
-		try:
-			mail = notifiche.email(mittente		= 'WiMegera <wimegera@gelma.net>',
-								   destinatario	= ['WiMegera <wimegera@gelma.net>'],
-								   oggetto 		= 'WiMegera: aggiornamenti Lug su Wikipedia',
-								   testo		= righe_della_mail,
-								   invia_subito	= True) # Se da Aggiornare, vedi Guida Intergalattica alla LugMap §4.1
-		except: # se fallisce l'invio stampo la mail, contando sul delivery di cron
-			print mail
-
-		salva_ultimo_id_inviato(id)
-
+	db.close()
 	#¹ il metodo run() mi restituisce un elenco di tuple, con i dettagli delle modifiche, come questa:
 	## (39770501, u'2011-04-08T09:15:08Z', u'Gelma', u'/* Collegamenti esterni */ Aggiunto dominio dedicato', 13101, [])
