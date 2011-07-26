ecfc958f6d763a193325430f8592858cfb083e59
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-07-27 01:46:52 +0200

    Sito: portato all'esterno il javascript di Piwik.
    Giusto per risparmiare una manciata di byte ad ogni richiesta

---
+++ js/piwik.js
@@ -0,0 +1,7 @@
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://pergamena.lugbs.linux.it/" : "http://pergamena.lugbs.linux.it/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
