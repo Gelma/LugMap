668da51f23a8a77888a63e66b4aa9f273af1d489
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-07-28 22:48:41 +0200

    Sito: aggiunte informazioni per i crawler
    Nulla di fondamentale. Un po' di informazioni di base per dare una mano
    ai crawler.
    In particolare abbiamo escluso la dir db/, giusto per evitare che
    vengano tornati i file raw in luogo delle pagine apposite.
    Ovviamente il tutto resta comunque accessibile nei vari modi.

--- funzioni.php
+++ funzioni.php
@@ -32,9 +32,10 @@ function lugheader ($title, $extracss = null, $extrajs = null) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it">
 <head>
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="language" content="italian" />
+  <meta name="robots" content="noarchive" />
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" />
   <link href="/css/main.css" rel="stylesheet" type="text/css" />
 
   <?php
