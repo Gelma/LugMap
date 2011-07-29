9c60e330d82cb649e4b475a04faaa3738add9d78
Roberto Guido <bob4job@gmail.com>
2011-07-29 03:36:05 +0200

    LugMap.it: revisionato footer

--- varie.php
+++ varie.php
@@ -87,21 +87,20 @@ function lugfooter () {
 <div id="footer">
   <table width="70%" align="center">
 	<tr><td>
-		<p class="helpMessage">Aiutaci a mantenere la LugMap aggiornata!</p>
 		<p class="helpMessage">
-		Segnalaci nuovi gruppi, cos&igrave; come errori ed omissioni, scrivendo
-		alla <a class="generalink" href="http://lists.linux.it/listinfo/lugmap">mailing list pubblica</a>,
-		oppure contattando direttamente Andrea Gelmini (telefonicamente al
-		<a class="generalink" href="tel:328-72-96-628">328-72-96-628</a>, via
-		<a class="generalink" href="mailto:andrea.gelmini@lugbs.linux.it">mail</a>, o attraverso
-		<a class="generalink" href="http://www.facebook.com/andrea.gelmini">Facebook</a>.)<br>
-		Puoi partecipare direttamente, sia alla stesura del codice che del database, sfruttando il
-		<a class="generalink" href="http://github.com/Gelma/LugMap">repository GitHub</a>.
-		Per saperne di pi&ugrave; &egrave; disponibile la
-		<a class="generalink" href="http://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">
-		Guida Intergalattica alla LugMap</a>.
+			Aiutaci a mantenere la LugMap aggiornata!
+			Segnalaci nuovi gruppi, cos&igrave; come errori ed omissioni, scrivendo
+			alla <a class="generalink" href="http://lists.linux.it/listinfo/lugmap">mailing list pubblica</a>!
+		</p>
 		<p class="helpMessage">
-		Te ne saremo eternamente grati!
+			Puoi partecipare direttamente, sia alla stesura del codice che del database, sfruttando il
+			<a class="generalink" href="http://github.com/Gelma/LugMap">repository GitHub</a>.
+			Per saperne di pi&ugrave; &egrave; disponibile la
+			<a class="generalink" href="http://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">Guida Intergalattica alla LugMap</a>.
+		</p>
+		<p class="helpMessage">
+			Te ne saremo eternamente grati!
+		</p>
 	  </td></tr>
   </table>
 </div>
