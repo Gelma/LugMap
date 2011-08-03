5118794edaf34f615572fa803fc913c42c58c310
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-08-04 00:14:15 +0200

    Sito: aggiunto Google Analytics
    Lo lascio attivo almeno temporaneamente per avere un riscontro
    rispetto a Piwik (pergamena.lugbs.linux.it), e per vedere di quanto
    le visite giornaliere che abbiamo influiscono sui risultati di ricerca.

--- funzioni.php
+++ funzioni.php
@@ -63,6 +63,19 @@ function lugheader ($title, $extracss = null, $extrajs = null) {
   </script>
 
   <title><?php echo $title; ?></title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3626063-13']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <!-- <a href="http://lugmap.linux.it/css/contact.php">select</a> -->
 <body>
