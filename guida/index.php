aa82ad6bcd0cf1a61929bf57b2998aa19cd40f92
Roberto Guido <bob4job@gmail.com>
2011-08-19 03:46:23 +0200

    Lugmap.Linux.it: revisionata interamente pagina della "Guida"

--- guida/index.php
+++ guida/index.php
@@ -2,15 +2,21 @@
 require_once ('../funzioni.php');
 lugheader ('Guida');
 ?>
-<table width="70%" align="center">
-	<tr><td>
-	<p>
-	Tutte le informazioni necessarie, sia per chi voglia apparire sul sito, sia per chi voglia
-	partecipare allo sviluppo del progetto, sono raccolte nella
-	<a href="http://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">Guida Intergalattica alla LugMap</a>.
-	</p>
-	</td></tr>
-</table>
+
+<div style="width: 70%; text-align: center; margin: auto">
+	<p>Vuoi apparire sul sito? Vuoi sapere come funziona il progetto? O meglio ancora, vuoi partecipare insieme a noi?</p>
+	<p>Don't panic! Consulta la...</p>
+	<div style="width: 60%; margin: auto">
+		<a href="https://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">
+			<!-- image courtesy: mattlemmon http://www.flickr.com/photos/mplemmon/231740358/ -->
+			<img src="http://farm1.static.flickr.com/85/231740358_4c2834347b.jpg" "="" style="float: left; width: 50%">
+			<span style="width: 50%; margin-top: 10px; text-align: center; word-wrap: break-word; float: right; font-size: 35px">Guida Intergalattica alla LugMap</span>
+		</a>
+	</div>
+
+	<div class="clear_spacer"></div>
+</div>
+
 <?php
   lugfooter ();
 ?>
