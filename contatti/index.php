e5c52f5fdd35d5ff59ccb60e401d19cd1e08fffc
Roberto Guido <bob4job@gmail.com>
2011-08-19 03:57:20 +0200

    Lugmap.Linux.it: revisionato layout pagina "Contatti"

--- contatti/index.php
+++ contatti/index.php
@@ -5,17 +5,25 @@ lugheader ('Contatti');
 <table width="70%" align="center">
 	<tr><td>
 		<p>I manovali della LugMap possono essere raggiunti nei seguenti modi contattando:
-		<ul>
-		<li>la <a href="http://lists.linux.it/listinfo/lugmap">mailing list</a> (si badi che pubblica &egrave; la lista, cos&igrave; come l'archivio);
-		<li><img src="andrea_gelmini.png">
-			<a class="generalink" href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a> telefonicamente al
-			<a class="generalink" href="tel:328-72-96-628">328/72-96-628</a>, via
-			<a class="generalink" href="mailto:andrea.gelmini@lugbs.linux.it">mail</a>, o attraverso
-			<a class="generalink" href="http://www.facebook.com/andrea.gelmini">Facebook</a>
-		<li><img src="roberto_guido.png">
-			<a class="generalink" href="mailto:bob4job@gmail.com">Roberto Guido</a> via
-			<a class="generalink" href="mailto:bob4job@gmail.com">email</a> o attraverso
-			<a class="generalink" href="http://www.facebook.com/MadBob">Facebook</a>
+		<ul class="spaced-list">
+				<li>
+					la <a href="http://lists.linux.it/listinfo/lugmap" class="generalink">mailing list</a> (si badi che pubblica è la lista, così come l'archivio)
+				</li>
+				<li>
+					<img src="andrea_gelmini.png" style="float: left">
+					<a href="mailto:andrea.gelmini@lugbs.linux.it" class="generalink">Andrea Gelmini</a><br>telefonicamente al
+					<a href="tel:328-72-96-628" class="generalink">328/72-96-628</a><br>via
+					<a href="mailto:andrea.gelmini@lugbs.linux.it" class="generalink">mail</a><br>o attraverso
+					<a href="http://www.facebook.com/andrea.gelmini" class="generalink">Facebook</a>
+					<div class="clear_spacer"></div>
+				</li>
+				<li>
+					<img style="float: left" src="roberto_guido.png">
+					<a class="generalink" href="mailto:bob4job@gmail.com">Roberto Guido</a><br>via
+					<a class="generalink" href="mailto:bob4job@gmail.com">mail</a><br>o attraverso
+					<a class="generalink" href="http://www.facebook.com/MadBob">Facebook</a>
+					<div class="clear_spacer"></div>
+				</li>
 		</ul>
 		</p>
 	</td></tr>
