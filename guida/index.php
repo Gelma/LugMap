0c877dbc8a96763ed7756aa630bd93e8aded75b3
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-08-19 10:00:54 +0200

    Sito: Guida: ridotto dimensione scritta
    Per evitare di spezzare la parola "Intergalattica"

--- guida/index.php
+++ guida/index.php
@@ -5,18 +5,18 @@ lugheader ('Guida');
 
 <div style="width: 70%; text-align: center; margin: auto">
 	<p>Vuoi apparire sul sito? Vuoi sapere come funziona il progetto? O meglio ancora, vuoi partecipare insieme a noi?</p>
-	<p>Don't panic! Consulta la...</p>
+	<p>Don't panic! Consulta la…</p>
 	<div style="width: 60%; margin: auto">
 		<a href="https://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">
 			<!-- image courtesy: mattlemmon http://www.flickr.com/photos/mplemmon/231740358/ -->
 			<img src="http://farm1.static.flickr.com/85/231740358_4c2834347b.jpg" "="" style="float: left; width: 50%">
-			<span style="width: 50%; margin-top: 10px; text-align: center; word-wrap: break-word; float: right; font-size: 35px">Guida Intergalattica alla LugMap</span>
+			<span style="width: 50%; margin-top: 10px; text-align: center; word-wrap: break-word; float: right; font-size: 25px">Guida Intergalattica alla LugMap</span>
 		</a>
 	</div>
 
 	<div class="clear_spacer"></div>
 </div>
-
+<br />
 <?php
   lugfooter ();
 ?>
