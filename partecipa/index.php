61f1f6793817705f1933ac1490739b5ff3f9ade3
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-08-25 19:57:17 +0200

    Sito: allineato la pagina Pagina partecipa al resto.
    Aggiornato anche il testo.

--- partecipa/index.php
+++ partecipa/index.php
@@ -4,14 +4,14 @@
 ?>
 <table width="70%" align="center">
 	<tr><td>
-	<p class="helpMessage">Aiutaci a mantenere la LugMap aggiornata!</p>
-	<p class="helpMessage">Segnalaci nuovi gruppi, cos&igrave; come errori ed omissioni, grazie
+	<p><b>Aiutaci a mantenere la LugMap aggiornata!</b></p>
+	<p>Segnalaci nuovi gruppi, cos&igrave; come errori ed omissioni, grazie
 	alla pagina dei <a class="generalink" href="/contatti">contatti</a>.</p>
-	<p class="helpMessage">Puoi partecipare direttamente, sia alla stesura del codice che del database, sfruttando il
+	<p>Puoi partecipare direttamente, sia alla stesura del codice che del database, sfruttando il
 	<a class="generalink" href="http://github.com/Gelma/LugMap">repository GitHub</a>.</p>
-	<p class="helpMessage">Per saperne di pi&ugrave; &egrave; disponibile la
+	<p>Puoi sapere tutto del progetto leggendo la
 	<a class="generalink" href="/guida/">Guida Intergalattica alla LugMap</a>.</p>
-	<p class="helpMessage">Te ne saremo eternamente grati!</p>
+	<p><b>Partecipa! Te ne saremo eternamente grati!</b></p>
 	</td></tr>
 </table>
 <?php
