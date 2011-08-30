bd61b45181a20af40d40d53198380844cc4f19ad
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-08-30 14:35:53 +0200

    Sito: aggiunta sezione di Propaganda
    Principalmente per evidenziare Lug-O-Matic.
    In un secondo tempo aggiungiamo banner, majette, ecc.

---
+++ propaganda/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once ('../funzioni.php');
+lugheader ('Propaganda');
+?>
+<table width="70%" align="center">
+	<tr><td>
+		<p>&Egrave; possibile diffondere il segnale della LugMap, su tutte le frequenze, nei seguenti metodi:
+		<ul class="spaced-list">
+				<li>
+					accedendo direttamente ai dati, come spiegato nel dettaglio sulla <a href="/guida/" class="generalink">Guida Intergalattica alla LugMap</a>;
+				</li>
+				<li>
+					includendo il codice generato dal widget web
+					<a href="/lug-o-matic/" class="generalink">lug-o-matic</a>
+					sul tuo sito.
+				</li>
+		</ul>
+		</p>
+	</td></tr>
+</table>
+<?php
+  lugfooter ();
+?>
