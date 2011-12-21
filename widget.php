84ddf4185deaeb3b345641520f3b17b12a7d70ac
Roberto Guido <bob4job@gmail.com>
2011-12-21 13:56:27 +0100

    LugMap.it: correzioni estetiche marginali nella pagina del widget web

--- widget.php
+++ widget.php
@@ -23,90 +23,92 @@ else
 	</p>
 </div>
 
-<fieldset style="width: 45%; float: left;">
-	<legend>Opzioni</legend>
+<div class="contents_wrapper">
+	<fieldset class="widget_options">
+		<legend>Opzioni</legend>
 
-	<p>
-		<label for="region">Regione</label>
+		<p>
+			<label for="region">Regione</label>
 
-		<select name="region">
-			<?php
-			foreach ($elenco_regioni as $simple => $name) {
-				if ($region == $simple)
-					$selected = ' selected="selected"';
-				else
-					$selected = '';
+			<select name="region">
+				<?php
+				foreach ($elenco_regioni as $simple => $name) {
+					if ($region == $simple)
+						$selected = ' selected="selected"';
+					else
+						$selected = '';
 
+					?>
+
+					<option value="<?php echo $simple ?>"<?php echo $selected ?>><?php echo $name; ?></option>
+
+					<?php
+				}
 				?>
 
-				<option value="<?php echo $simple ?>"<?php echo $selected ?>><?php echo $name; ?></option>
+				<option value="all">Tutti i LUG</option>
+			</select>
+		</p>
 
-				<?php
-			}
-			?>
+		<br />
 
-			<option value="all">Tutti i LUG</option>
-		</select>
-	</p>
+		<p>
+			<label for="width">Larghezza</label>
+			<input name="width" type="text" value="200" size="4" />px
+		</p>
 
-	<br />
+		<p>
+			<label for="border">Bordo</label>
+			<input name="border" type="text" value="3" size="4" />px
+		</p>
 
-	<p>
-		<label for="width">Larghezza</label>
-		<input name="width" type="text" value="200" size="4" />px
-	</p>
+		<p>
+			<label for="head">Mostra Header</label>
+			<input name="head" type="checkbox" checked="yes" />
+		</p>
 
-	<p>
-		<label for="border">Bordo</label>
-		<input name="border" type="text" value="3" size="4" />px
-	</p>
+		<p class="depends_on_header">
+			<label for="head_color">Colore Header</label>
+			<input name="head_color" type="color" value="#000080" data-text="hidden" data-hex="true" style="height: 15px; width: 20px;" />
+		</p>
 
-	<p>
-		<label for="head">Mostra Header</label>
-		<input name="head" type="checkbox" checked="yes" />
-	</p>
+		<p class="depends_on_header">
+			<label for="head_text_color">Colore Testo Header</label>
+			<input name="head_text_color" type="color" value="#FFFFFF" data-text="hidden" data-hex="true" style="height: 15px; width: 20px;" />
+		</p>
 
-	<p class="depends_on_header">
-		<label for="head_color">Colore Header</label>
-		<input name="head_color" type="color" value="#000080" data-text="hidden" data-hex="true" style="height: 15px; width: 20px;" />
-	</p>
+		<p>
+			<label for="foot">Mostra Footer</label>
+			<input name="foot" type="checkbox" checked="yes" />
+		</p>
 
-	<p class="depends_on_header">
-		<label for="head_text_color">Colore Testo Header</label>
-		<input name="head_text_color" type="color" value="#FFFFFF" data-text="hidden" data-hex="true" style="height: 15px; width: 20px;" />
-	</p>
+		<br />
 
-	<p>
-		<label for="foot">Mostra Footer</label>
-		<input name="foot" type="checkbox" checked="yes" />
-	</p>
+		<p>
+			<p>Questa opzione puo' essere selezionata per ottenere una immagina statica anziche' un blocco di JavaScript: utile per embeddare il widget all'interno di siti che non consentono l'inclusione di codice HTML complesso, tipo Wordpress.com</p>
 
-	<br />
+			<label for="image">Genera Immagine</label>
+			<input name="image" type="checkbox" />
+		</p>
+	</fieldset>
 
-	<p>
-		<p>Questa opzione puo' essere selezionata per ottenere una immagina statica anziche' un blocco di JavaScript: utile per embeddare il widget all'interno di siti che non consentono l'inclusione di codice HTML complesso, tipo Wordpress.com</p>
+	<div style="width: 45%; float: right; text-align: center;">
+		<div class="preview">
+			<!-- Non usare l'URL con l'host, qui, altrimenti usando widget.lugmap.it o lugmap.it non tornano i conti con la same-origin policy -->
+			<iframe id="lugmap" src="forge/lug-o-matic/widget.php?region=<?php echo $region ?>&amp;format=html" onLoad="calcSize();" width="210px" scrolling="no" frameborder="0"></iframe>
+		</div>
 
-		<label for="image">Genera Immagine</label>
-		<input name="image" type="checkbox" />
-	</p>
-</fieldset>
+		<br />
+		<br />
+		<br />
 
-<div style="width: 45%; float: right; text-align: center;">
-	<div class="preview">
-		<!-- Non usare l'URL con l'host, qui, altrimenti usando widget.lugmap.it o lugmap.it non tornano i conti con la same-origin policy -->
-		<iframe id="lugmap" src="forge/lug-o-matic/widget.php?region=<?php echo $region ?>&amp;format=html" onLoad="calcSize();" width="210px" scrolling="no" frameborder="0"></iframe>
+		<p>Copia e incolla questo codice nella tua pagina web!</p>
+
+		<textarea class="code" cols="45" rows="10"><?php echo htmlentities (
+		'<script type="text/javascript" src="' . $main_url . '/forge/lug-o-matic/widget.php?region=' . $region . '"></script>
+		<img id="lugmap" src="' . $main_url . '/forge/lug-o-matic/placeholder.png" onload="renderLugMap();" />') ?>
+		</textarea>
 	</div>
-
-	<br />
-	<br />
-	<br />
-
-	<p>Copia e incolla questo codice nella tua pagina web!</p>
-
-	<textarea class="code" cols="45" rows="10"><?php echo htmlentities (
-	'<script type="text/javascript" src="' . $main_url . '/forge/lug-o-matic/widget.php?region=' . $region . '"></script>
-	<img id="lugmap" src="' . $main_url . '/forge/lug-o-matic/placeholder.png" onload="renderLugMap();" />') ?>
-	</textarea>
 </div>
 
 <?php do_foot (); ?>
