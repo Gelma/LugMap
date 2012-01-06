2cfb171dd5d31f62fcf5b58310e326d4d45aa57d
Andrea Gelmini <andrea.gelmini@gelma.net>
2012-01-06 21:02:27 +0100

    sito: aggiornato motto sulla pagina della guida

--- guida/index.php
+++ guida/index.php
@@ -24,8 +24,8 @@ lugheader ('Guida');
 	<p>Vuoi apparire sul sito? Vuoi sapere come funziona il progetto? O meglio ancora, vuoi partecipare insieme a noi?</p>
 	<p style="font-size: 200%">Don't panic! Consulta la…</p>
 	<p style="font-size: 300%"><a href="https://github.com/Gelma/LugMap/raw/docs/Guida_Intergalattica_Alla_LugMap.pdf">Guida Intergalattica alla LugMap</a></p>
-	<p>Tutti, ma proprio tutti, i dettagli su chi, cosa, come mantiene la LugMap, sui files raw pubblici, sugli script di automazione interna.</p>
-	<p>Bonus track: istruzioni su come rimettere in piedi l'intera struttura in caso di esplosione termonucleare che distrugga i server ed evapori gli amministratori attuali.</p>
+	<p>Tutti, ma proprio tutti, i dettagli su chi, cosa, come mantiene la LugMap, dai file pubblici agli script di automazione interna.</p>
+	<p>Bonus track: le istruzioni su come rimettere in piedi l'intera struttura in caso di esplosione termonucleare che distrugga i server e vaporizzi gli amministratori attuali.</p>
 </div>
 
 <?php
