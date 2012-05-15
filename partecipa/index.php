f877b2b483460911ff34f7bbee730a6b6dab5b57
Roberto Guido <bob4job@gmail.com>
2012-05-16 01:05:29 +0200

    LugMap.Linux.it: specificate nella pagina "Partecipa" le licenze con cui sono rilasciati sito e dati

--- partecipa/index.php
+++ partecipa/index.php
@@ -25,7 +25,9 @@
 	<p>Segnalaci nuovi gruppi, cos&igrave; come errori ed omissioni, grazie
 	alla pagina dei <a class="generalink" href="/contatti">contatti</a>.</p>
 	<p>Puoi partecipare direttamente, sia alla stesura del codice che del database, sfruttando il
-	<a class="generalink" href="http://github.com/Gelma/LugMap">repository GitHub</a>.</p>
+	<a class="generalink" href="http://github.com/Gelma/LugMap">repository GitHub</a>.
+	Il codice sorgente del sito &egrave; distribuito in licenza <a href="http://www.gnu.org/licenses/agpl-3.0-standalone.html">AGPLv3</a>, mentre i dati sono
+	pubblicati in licenza <a href="http://creativecommons.org/publicdomain/zero/1.0/legalcode">CC0</a>.</p>
 	<p>Puoi sapere tutto del progetto leggendo la
 	<a class="generalink" href="/guida/">Guida Intergalattica alla LugMap</a>,
 	sequendo la <a href="http://lists.linux.it/listinfo/lugmap">mailing list</a>
